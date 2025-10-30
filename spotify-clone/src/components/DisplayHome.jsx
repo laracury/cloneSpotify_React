6c7322f5356d9b869e9c235e1bf2a955045f248e
Lara Cury <laracury2@gmail.com>
2025-10-30 15:57:32 -0300

    🚚Criado a rota para os albuns

--- spotify-clone/src/components/DisplayHome.jsx
+++ spotify-clone/src/components/DisplayHome.jsx
@@ -3,6 +3,7 @@ import Navbar from './Navbar'
 import { albumsData } from '../assets/assets'
 import AlbumItem from './AlbumItem'
 import { songsData } from '../assets/assets'
+import SongsItem from './SongsItem'
 
 const DisplayHome = () => {
   return (
@@ -14,7 +15,12 @@ const DisplayHome = () => {
                 {albumsData.map((item, index)=>(<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
             </div>
         </div>
-        
+        <div className='mb-4'>
+            <h1 className='my-5 font-bold text-2xl'>Today's Biggest Hits</h1>
+            <div className='flex overflow-auto'>
+                {songsData.map((item,index)=>(<SongsItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
+            </div>
+        </div>
     </>
   )
 }
